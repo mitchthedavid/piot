3fe662aed44baa2f38b69c7eb0d0ba8e5376dc22
mitchell <pehorami@gmail.com>
2019-11-03 10:03:45 -0500

    Update BEAMBREAKTEST.py
    now produces pinout, checks pins

--- prototypes/BEAMBREAKTEST.py
+++ prototypes/BEAMBREAKTEST.py
@@ -1,20 +1,43 @@
 
-# import only system from os 
-from os import system, name 
-  
+# import call method from subprocess module 
+from subprocess import call 
+
 # import sleep to show output for some time period 
-from time import sleep 
-  
-# define our clear function 
+import time
+import os 
+
+# define clear function 
 def clear(): 
-  
-    system( 'cls' )
-  
-# print out some text 
-print('hello geeks\n'*10) 
-  
-# sleep for 2 seconds after printing output 
-sleep(2) 
-  
-# now call function we defined above 
-clear() 
+    # check and make call for specific operating system 
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+#import MySQLdb
+#db = MySQLdb.connect("localhost", "pi", "raspberry", "button")
+#curs=db.cursor()
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+print("PIN 7 IS INPUT")
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+print("PIN 8 IS OUTPUT")
+os.system('pinout')
+myVar=input("PRESS ENTER TO CONTINUE...\n\n\n")
+print("listening")
+counter=0
+while True: # Run forever
+    if GPIO.input(7) == GPIO.LOW:
+        GPIO.output(8, GPIO.LOW) # Turn on
+        print("pin 7 is low")
+
+    if GPIO.input(7) == GPIO.HIGH:
+        GPIO.output(8, GPIO.HIGH)
+        print("pin 7 is high")
+
+    #counter+=1
+    #if counter%50 == 0:
+        #clear()    
+    time.sleep(2)
+    clear()
